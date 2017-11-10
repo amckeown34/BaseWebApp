5f43804fd35617fda211fde9befdf65d78b1e57b
amckeown34 <amckeown34@qub.ac.uk>
2017-11-10 17:02:00 +0000

    Maybe it will wwork this time!

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,17 @@
 
 function myFunction() {
+
+	app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.get('/about', function(request, response) {
+  response.render('pages/about');
+});
+
+app.get('/projects', function(request, response) {
+  response.render('pages/projects');
+});
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // jQuery can do a lot of crazy stuff so make sure to google around to find out more
 
